d63e93fe0de48de5e93f35527c2f7fb75f1bd67d
Abdulqadir Bohra <98mbohra@gmail.com>
2019-04-30 12:15:20 +1200

    Initial commit

---
+++ app/src/main/java/com/example/bohra/savvysavingappworking/SetIncomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.bohra.savvysavingappworking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetIncomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.income_set_activity);
+    }
+}
